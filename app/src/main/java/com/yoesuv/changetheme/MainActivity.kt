922214e9470421dfd997ee783dd7561f09ced0ad
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2018-10-18 15:48:31 +0700

    :pencil: setup activity

--- app/src/main/java/com/yoesuv/changetheme/MainActivity.kt
+++ app/src/main/java/com/yoesuv/changetheme/MainActivity.kt
@@ -2,11 +2,29 @@ package com.yoesuv.changetheme
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.CompoundButton
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        setupSwitch()
+    }
+
+    private fun setupSwitch(){
+        switchMain.setOnCheckedChangeListener { _: CompoundButton, b: Boolean ->
+            Log.d("result_debug","MainActivity # setupSwitch $b")
+            if (b) {
+                setTheme(R.style.AppThemeLight)
+            } else {
+                setTheme(R.style.AppThemeDark)
+            }
+
+        }
     }
 }
