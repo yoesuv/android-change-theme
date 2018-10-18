032cd07ecf2fdce8852583cb2c85685b7a775e63
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2018-10-18 16:23:44 +0700

    :sparkles: change light & dark theme

--- app/src/main/java/com/yoesuv/changetheme/AppConstants.kt
+++ app/src/main/java/com/yoesuv/changetheme/AppConstants.kt
@@ -3,4 +3,5 @@ package com.yoesuv.changetheme
 object AppConstants {
     const val RESULT_DEBUG = "result_debug"
     const val PREFERENCES_NAME = "change.theme.pref"
+    const val PREFERENCES_THEME_LIGHT_DARK = "preferences_theme_light_dark"
 }
--- app/src/main/java/com/yoesuv/changetheme/MainActivity.kt
+++ app/src/main/java/com/yoesuv/changetheme/MainActivity.kt
@@ -1,30 +1,43 @@
 package com.yoesuv.changetheme
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.CompoundButton
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private var theme: Boolean? = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        theme = App.prefHelper?.getBoolean(AppConstants.PREFERENCES_THEME_LIGHT_DARK)
+        switchLightDarkTheme(theme)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        supportActionBar?.elevation = 8F
 
         setupSwitch()
     }
 
-    private fun setupSwitch(){
+    private fun setupSwitch() {
+        switchMain.isChecked = theme!!
         switchMain.setOnCheckedChangeListener { _: CompoundButton, b: Boolean ->
-            Log.d(AppConstants.RESULT_DEBUG,"MainActivity # setupSwitch $b")
-            if (b) {
-                setTheme(R.style.AppThemeLight)
-            } else {
-                setTheme(R.style.AppThemeDark)
-            }
+            App.prefHelper?.setBoolean(AppConstants.PREFERENCES_THEME_LIGHT_DARK, b)
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
 
+    private fun switchLightDarkTheme(value: Boolean?) {
+        if (value!!) {
+            setTheme(R.style.AppThemeDark)
+        } else {
+            setTheme(R.style.AppThemeLight)
         }
     }
 }
